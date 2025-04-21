28a2e0caaa3f74bc2f55fe5dddebb1ef2449655b
Imamk48 <imamardiperdana48@gmail.com>
2025-04-21 12:36:14 +0800

    Add files via upload

--- Modul 1  Pra-pratikum/pre-1 (PERULANGAN)/Function.py
+++ Modul 1  Pra-pratikum/pre-1 (PERULANGAN)/Function.py
@@ -111,3 +111,22 @@ def sapa():
 
 # memanggil function
 sapa()
+
+# membuat variabel global
+nama = "UNDIKMA"
+versi = "1.0.0"
+
+def help():
+    # ini variabel lokal
+    nama = "Programku"
+    versi = "1.0.2"
+    # mengakses variabel lokal
+    print("Nama: %s" % nama)
+    print("Versi: %s" % versi)
+
+# mengakses variabel global
+print("Nama: %s" % nama)
+print("Versi: %s" % versi)
+
+# memanggil fungsi help()
+help()
